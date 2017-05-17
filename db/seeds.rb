0047b025a44692c834e9593bc04903c49693567f
k-boiteau <boiteau.kev@gmail.com>
2017-05-17 12:32:28 +0200

    instance of restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "chinese"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "italian"
+  },
+  {
+    name:         "L'orillon bar",
+    address:      "rue de l'orillon 75011 Paris",
+    category:     "french"
+  },
+  {
+    name:         "Le dauphiné",
+    address:      "avenue de la Republique 75011 Paris",
+    category:     "japanese"
+  }
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
